7f8b992f1f31d4e1c7f1e707a34b00efdafe9b6f
caio poleza dos santos <caio_p_santos@estudante.sesisenai.org.br>
2025-03-20 10:08:14 -0300

    add-atividade9

---
+++ script.js
@@ -0,0 +1,53 @@
+/*console.log('avtividade');
+
+let numero = prompt('digite seu numero:');
+
+function verificarpar(numero){
+   if(numero % 2 === 0){
+     console.log(numero + " e igual a par.");
+   }else{
+    console.log(numero + " e igual a impar.");
+   }
+
+}
+ 
+verificarpar(numero);
+*/
+/*
+let n1 = parseFloat(prompt('digite um numero:'))
+let n2 = parseFloat(prompt('digite um numero:'))
+
+
+
+function calcular(num1,num2){
+
+ const soma = num1 + num2;
+ const subtracao = num1 - num2;
+ const multiplicacao = num1 * num2;
+ const divisao = num1 / num2;
+
+ console.log(soma)
+ console.log(subtracao)
+ console.log(multiplicacao)
+ console.log(divisao)
+ if (num2 === 0) {
+    console.log('não da')
+ } else{
+ const divisao = num1 / num2;
+ console.log(divisao)
+
+ }
+
+}
+
+
+
+calcular(n1,n2);
+
+*/
+
+calcular(n1,n2)
+
+let n = 11
+let x = 0
+ while
